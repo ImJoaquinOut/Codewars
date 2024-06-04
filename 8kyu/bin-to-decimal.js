ed5094a51950d2d95401e52232f7a4ea09ce8131
Joaquin Hernandez <joaquinqinghernandez@gmail.com>
2024-06-03 21:04:43 -0500

    Bin to Decimal codewars

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,18 @@
+//Description
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// BINARYFUNDAMENTALS
+
+//My solution
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
+
+//Other solution
+const binToDec = bin => parseInt(bin,2);
+
+//Best practices
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
