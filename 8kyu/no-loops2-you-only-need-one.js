4cf3b533883148f2e0689dfeecd10716645182e0
Joaquin Hernandez <joaquinqinghernandez@gmail.com>
2024-07-15 09:04:02 -0500

    No Loops 2-You only need one codewars

---
+++ 8kyu/no-loops2-you-only-need-one.js
@@ -0,0 +1,28 @@
+//Description
+
+// *** No Loops Allowed ***
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+// No Loops 1 - Small enough?
+
+// FUNDAMENTALSARRAYS
+
+//My solution
+function check(a,x){
+    return a.includes(x) ? true : false;
+  };
+
+//Other solution
+function check(a, x) {
+    return a.includes(x);
+  }
+
+//Best practices
+function check(a,b){
+    return a.includes(b);
+  };
